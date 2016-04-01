64c1e59608dcb6264006493bca5fe3edd477370b
Maciej Sawicki <maciej.sawicki@codilime.com>
2016-04-01 22:44:22 +0200

    added logstash

--- app.py
+++ app.py
@@ -1,6 +1,25 @@
 from flask import Flask
+import logging
+import logstash
+import sys
+
+host = '192.168.213.4'
+
+test_logger = logging.getLogger('python-logstash-logger')
+test_logger.setLevel(logging.INFO)
+test_logger.addHandler(logstash.TCPLogstashHandler(host, 5000, version=1))
+# test_logger.addHandler(logstash.TCPLogstashHandler(host, 5959, version=1))
+
+test_logger.error('python-logstash: test logstash error message.')
+test_logger.info('python-logstash: test logstash info message.')
+test_logger.warning('python-logstash: test logstash warning message.')
+
+# add extra field to logstash message
 
 app = Flask(__name__)
+
+
+test_logger.info('python-logstash')
 
 @app.route("/")
 def hello():
